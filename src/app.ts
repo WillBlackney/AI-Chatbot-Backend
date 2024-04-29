34c6a010ebcf3482fa30e5da124c089e534e9c99
William Blackney <william.blackney@hotmail.com>
2024-04-29 21:53:33 +0200

    cors update

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ config();
 const app = express();
 
 // Middlewares
-app.use(cors({origin: "http://localhost:5173", credentials: true}));
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET))
 app.use(morgan("dev")); // remove from production builds
