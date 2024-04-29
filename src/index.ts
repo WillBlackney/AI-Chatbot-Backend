da1c8264f3a6c7a696c62597261eadbbbb0656e4
William Blackney <william.blackney@hotmail.com>
2024-04-29 23:08:04 +0200

    db connection error handling

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,12 @@ import { connectToDatabase } from "./db/connection.js";
 const port = process.env.PORT || 5000;
 connectToDatabase()
   .then(() => {
-    app.listen(port), () => console.log("Server open and connected to database!");
+    app.listen(port, () => {
+      console.log("Server open and connected to database!");
+    });
   })
-  .catch((error) => console.log(error));
+  .catch((error) => {
+    console.error("Error connecting to database:", error);
+    process.exit(1); // Exit the process with a non-zero exit code to indicate failure
+  });
+
